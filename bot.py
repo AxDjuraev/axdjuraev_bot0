80dcbbff78324dc6907b57d20174a7084ef44508
root <axdjuraev@gmail.com>
2022-07-15 15:09:31 +0000

    modify bot functions

--- bot.py
+++ bot.py
@@ -1,16 +1,27 @@
 
 from telebot import TeleBot
 from time import sleep
-API = '5306057698:AAFtg7O014soJoSreLDUXYZEOEN_liKTGwk';
+from pytube import YouTube
+
+API = '5306057698:AAFtg7O014soJoSreLDUXYZEOEN_liKTGwk'
 bot = TeleBot(API)
 @bot.message_handler()
+def downloadYoutubeVideo(video_link):
+  youtube_video = YouTube(link)
+  streams = youtube_video.streams.filter(res='360p', progressive="True")
+  file_name = streams.first().download()
+  return file_name
 def doSomething(person):
+  id = person.chat.id 
   try:
-    id = person.chat.id 
     message = person.text
-    bot.send_message(id,f'You said: {message}')
-  except:
-    pass
+    strippedMessage = message.strip()
+    if not strippedMessage.startswith('/'):
+      bot.send_message(id, f'function not found. Try send message "/help"')
+      return
+    
+  except Exception as exception:
+    bot.send_message(id, f'error: "{str(exception)}" and try again.')
 # id = '1722229628'
 
 # while True:
