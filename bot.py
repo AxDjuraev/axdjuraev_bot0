60e580bdca0fcb1a4ce6f3b8216bc781061d600a
root <axdjuraev@gmail.com>
2022-07-15 13:07:15 +0000

    modify bot functions

---
+++ bot.py
@@ -0,0 +1,15 @@
+
+from telebot import TeleBot
+from time import sleep
+API = '5306057698:AAFtg7O014soJoSreLDUXYZEOEN_liKTGwk';
+bot = TeleBot(API)
+@bot.message_handler()
+def doSomething(person):
+  id = person.chat.id 
+  message = person.text
+  bot.send_message(id,f'You said: {message}')
+
+while True:
+  bot.send_message(id, 'bot working...')
+  sleep(4)
+# bot.polling()
