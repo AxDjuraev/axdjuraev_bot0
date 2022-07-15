75d00f6982031a680e92570365255cf675504e00
root <axdjuraev@gmail.com>
2022-07-15 13:34:19 +0000

    modify bot functions

--- bot.py
+++ bot.py
@@ -5,13 +5,16 @@ API = '5306057698:AAFtg7O014soJoSreLDUXYZEOEN_liKTGwk';
 bot = TeleBot(API)
 @bot.message_handler()
 def doSomething(person):
-  id = person.chat.id 
-  message = person.text
-  bot.send_message(id,f'You said: {message}')
+  try:
+    id = person.chat.id 
+    message = person.text
+    bot.send_message(id,f'You said: {message}')
+  except:
+    pass
+# id = '1722229628'
 
-id = '1722229628'
-
-while True:
-  bot.send_message(id, 'bot working...')
-  sleep(6)
-# bot.polling()
+# while True:
+#   bot.send_message(id, 'bot working...')
+#   sleep(6)
+if __name__ == "__main__":
+  bot.polling()
