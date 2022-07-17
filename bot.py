5ec958d9ae5c072d898ec7ce77ecafd0bd504574
root <axjuraev@gmail.com>
2022-07-17 09:39:02 +0000

    trying add webdriver functions.

--- bot.py
+++ bot.py
@@ -3,9 +3,9 @@ from telebot import TeleBot
 from time import sleep
 from pytube import YouTube
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
+# from webdriver_manager.chrome import ChromeDriverManager
 
-driver = webdriver.Chrome(ChromeDriverManager().install())
+# driver = webdriver.Chrome(ChromeDriverManager().install())
 
 API = '5306057698:AAFtg7O014soJoSreLDUXYZEOEN_liKTGwk'
 bot = TeleBot(API)
